2b990a3cf71fe82c2907cff8a8fcef60de4e629e
Chris Rowles <me@rowles.ch>
2021-03-17 12:28:24 +0000

    initial commit

---
+++ bot.py
@@ -0,0 +1,129 @@
+import os
+import json
+import asyncio
+import requests
+import tabulate
+import discord
+from discord.ext import commands, tasks
+import subprocess
+import settings
+
+from cogs.maincog import MainCog
+
+URL = os.environ.get('SYSAPI_URL')
+TOKEN = os.environ.get('DISCORD_TOKEN')
+
+description = '''System Monitor'''
+bot = commands.Bot(command_prefix='!', description=description)
+bot.add_cog(MainCog(bot=bot, url=URL))
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+
+@bot.command()
+async def uptime(ctx):
+    """Fetch network uptime"""
+    response = requests.get(URL)
+    system = response.json()
+    uptime = system['data']['platform']['uptime']
+
+    message = "Network has been up for " + system['data']['platform']['uptime']
+
+    await ctx.send(message)
+
+    
+@bot.command()
+async def iptables(ctx):
+    """Fetch active iptables rules"""
+    data = subprocess.check_output(["sudo", "iptables", "-L"])
+
+    await ctx.send('```\n' + data.decode("utf-8") + '\n```')
+
+    
+@bot.command()
+async def jailer(ctx, jail, action, ip):
+    """Ban/unban an IP address"""
+    data = subprocess.check_output(["sudo", "fail2ban-client", "set", jail, action, ip])
+    actioned = "banned" if action == "banip" else "unbanned"
+
+    message = "**" + ip + "** has been " + actioned + ", please run `!iptables` to confirm rules"
+
+    await ctx.send(message)
+
+
+@bot.command()
+async def unban(ctx, jail, ip):
+    """Unban an IP address (shorthand unban action for !jailer)"""
+    data = subprocess.check_output(["sudo", "fail2ban-client", "set", jail, "unbanip", ip])
+
+    message = "**" + ip + "** has been unbanned, please run `!iptables` to confirm rules"
+
+    await ctx.send(message)
+
+
+@bot.command()
+async def ban(ctx, jail, ip):
+    """Ban an IP address (shorthand ban action for !jailer)"""
+    data = subprocess.check_output(["sudo", "fail2ban-client", "set", jail, "banip", ip])
+
+    message = "**" + ip + "** has been banned, please run `!iptables` to confirm rules"
+
+    await ctx.send(message)
+
+
+@bot.command()
+async def top(ctx):
+    """Fetch top process consuming most memory"""
+    response = requests.get(URL)
+    system = response.json()
+    process = system['data']['processes'][0]
+
+    message = "Top process is " + process['name'].capitalize() + ' - ' + str(process['mem']) + 'MiB  - Owned by ' + process['username']
+
+    await ctx.send(message)
+
+
+@bot.command()
+async def sys(ctx, metric):
+    """Fetch system metrics for cpu, mem or disk"""
+    response = requests.get(URL)
+    system = response.json()
+
+    if metric == "raw":
+        data = json.dumps(system['data'], indent=2)
+
+        await ctx.send('```\n' + data + '\n```')
+    else:
+        if metric == "cpu":
+            data = system['data']['cpu']
+
+            embedlist = discord.Embed(title='System', description='cpu statistics')
+            embedlist.add_field(name='Temp', value=str(data['temp']) + '°c')
+            embedlist.add_field(name='Usage', value=str(data['usage']) + '%')
+            embedlist.add_field(name='Freq', value=str(data['freq']) + 'MHz')
+
+            await ctx.send(embed=embedlist)
+        elif metric == "htop":
+            data = system['data']['processes']
+
+            header = data[0].keys()
+            rows =  [x.values() for x in data]
+
+            await ctx.send('```\n' + tabulate.tabulate(rows, header) + '\n```')
+        else:
+            data = system['data'][metric]
+
+            embedlist = discord.Embed(title='System', description=str(metric) + ' statistics')
+            embedlist.add_field(name='Used', value=str(data['used']) + ' GB')
+            embedlist.add_field(name='Free', value=str(data['free']) + ' GB')
+            embedlist.add_field(name='Total', value=str(data['total']) + ' GB')
+
+            await ctx.send(embed=embedlist)
+
+
+bot.run(TOKEN)
---
+++ cogs/maincog.py
@@ -0,0 +1,53 @@
+import requests
+import discord
+import asyncio
+from discord.ext import commands, tasks
+
+class MainCog(commands.Cog):
+    def __init__(self, bot, url):
+        self.index = 0
+        self.bot = bot
+        self.url = url
+        self.channel = None
+        self.printer.start()
+
+
+    def cog_unload(self):
+        self.printer.cancel()
+
+
+    @tasks.loop(seconds=1800)
+    async def printer(self):
+        self.channel = self.bot.get_channel(821440456788541522)
+
+        if self.channel is not None:
+            response = requests.get(self.url)
+            system = response.json()
+            data = system['data']
+
+            if len(data['processes']) > 0:
+                await self.top(data['processes'][0])
+                await asyncio.sleep(5)
+
+            await self.sys(data)
+
+
+    async def top(self, process):
+        message = "Top process is " + process['name'].capitalize() + ' - ' + str(process['mem']) + 'MiB  - Owned by ' + process['username']
+
+        await self.channel.send(message)
+
+
+    async def sys(self, data):
+        embedlist = discord.Embed(title='System', description='Overview')
+        embedlist.add_field(name='Temp', value=str(data['cpu']['temp']) + ' °c')
+        embedlist.add_field(name='CPU', value=str(data['cpu']['usage']) + '%')
+        embedlist.add_field(name='Memory', value=str(data['mem']['percent']) + '%')
+
+        await self.channel.send(embed=embedlist)
+
+
+    @printer.before_loop
+    async def before_printer(self):
+        print('waiting...')
+        await self.bot.wait_until_ready()
