0e8c6a3ef4a71e905c9e505f88c0d0e366963308
Chris Rowles <me@rowles.ch>
2023-03-07 16:18:54 +0000

    latest changes

--- bot.py
+++ bot.py
@@ -1,27 +1,23 @@
 from cogs.commands import *
 from cogs.looped import *
+from discord import Intents
 from discord.ext import commands, tasks
 from settings import *
 
-
 description = """System Monitor"""
-bot = commands.Bot(command_prefix='!', description=description)
+bot = commands.Bot(command_prefix='!', description=description, intents=Intents.all())
 
-# Command handlers
-bot.add_cog(CommandErrorHandler(bot))
-bot.add_cog(NetCommandHandler(bot))
-bot.add_cog(SecurityCommandHandler(bot))
-bot.add_cog(SystemCommandHandler(bot, url=URL))
-bot.add_cog(MiscCommandHandler(bot, user=USER_ID))
+async def main():
+    await bot.add_cog(CommandErrorHandler(bot))
+    await bot.add_cog(NetCommandHandler(bot))
+    await bot.add_cog(SecurityCommandHandler(bot))
+    await bot.add_cog(SystemCommandHandler(bot, url=URL))
+    await bot.add_cog(MiscCommandHandler(bot, user=USER_ID))
 
-# Looped
-bot.add_cog(SystemReporting(bot=bot, url=URL, user=USER_ID, channel=CHANNEL_ID))
+    # Looped
+    await bot.add_cog(SystemReporting(bot=bot, url=URL, user=USER_ID, channel=CHANNEL_ID))
 
-
-@bot.command()
-async def test(ctx):
-    """Test command, make sure bot is responding"""
-    await ctx.send("%s Test Success" % USER_ID)
-
-
-bot.run(TOKEN)
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
+    bot.run(TOKEN)
--- cogs/commands.py
+++ cogs/commands.py
@@ -55,121 +55,18 @@ class CommandErrorHandler(commands.Cog):
 class SecurityCommandHandler(commands.Cog, name="Security Commands"):
     def __init__(self, bot):
         self.bot = bot
-        self.breach_id = 948990582510997595
-        self.breach.start()
-
 
     def cog_unload(self):
         self.system.cancel()
 
-
     @commands.command(name='hibp')
     async def hibp(self, ctx, account):
-        """Fetch breached account data"""
-        await ctx.send('Obtaining data breaches linked to ' + str(account) + '...')
-
-        response = requests.get('https://breach.api.rowles.ch/v1/breachedaccounts/' + str(account))
-        breaches = response.json()
-        message = breaches['message']
-
-        if breaches['data'] is None:
-            embedlist = discord.Embed(title='Data Breach Service', color=discord.Color.green())
-            embedlist.add_field(name='Result', value=message)
-
-            await ctx.send(embed=embedlist)
-            return
-        else:
-            embedlist = discord.Embed(title='Data Breach Service', color=discord.Color.red())
-            embedlist.add_field(name='Result', value=message + ' Listing breaches below, please wait...')
-
-            await ctx.send(embed=embedlist)
-            await asyncio.sleep(3)
-
-        for index, breach in enumerate(breaches['data']):
-            if (breach['domain']) != '':
-                domain = "[" + str(breach['domain']) + "](https://" + str(breach['domain']) + ")"
-            else:
-                domain = "N/A"
-
-            date_format_from = '%Y-%m-%d %H:%M:%S'
-            date_format_to = '%a %d %B %Y'
-
-            breach_date = datetime.strptime(str(breach['breach_date']), date_format_from)
-            breach_date = datetime.strftime(breach_date, date_format_to)
-
-            added_date = datetime.strptime(str(breach['added_date']), date_format_from)
-            added_date = datetime.strftime(added_date, date_format_to)
-
-            embedlist = discord.Embed(title='Breach #' + str(index+1), color = discord.Color.red())
-            output = "Domain: " + domain + "\nOccured: " + breach_date + "\nReported: " + added_date
-            embedlist.add_field(name=str(breach['title']), value=output, inline=False)
-
-            await ctx.send(embed=embedlist)
-            await asyncio.sleep(1)
-
+        await asyncio.sleep(1)
 
     @tasks.loop(seconds=86400)
     async def breach(self):
-        self.channel = self.bot.get_channel(self.breach_id)
-        await self.channel.send('================ **BREACH SERVICE REPORT START** ================')
-        await asyncio.sleep(3)
+        await asyncio.sleep(5)
 
-        accounts = [
-            'christopher.rowles@outlook.com',
-            'chrisrowles93@hotmail.com',
-            'me@rowles.ch',
-            'crowles.sdx@gmail.com',
-            'rowlesh@aol.com'
-        ]
-
-        for account in accounts:
-            """Fetch breached account data"""
-            await self.channel.send('Obtaining data breaches linked to ' + str(account) + '...')
-
-            response = requests.get('https://breach.api.rowles.ch/v1/breachedaccounts/' + str(account))
-            breaches = response.json()
-            message = breaches['message']
-
-            processing = True
-            if breaches['data'] is None:
-                embedlist = discord.Embed(title='Data Breach Service', color=discord.Color.green())
-                embedlist.add_field(name='Result', value=message)
-
-                await self.channel.send(embed=embedlist)
-                processing = False
-            else:
-                embedlist = discord.Embed(title='Data Breach Service', color=discord.Color.red())
-                embedlist.add_field(name='Result', value=message + ' Listing breaches below, please wait...')
-
-                await self.channel.send(embed=embedlist)
-                await asyncio.sleep(3)
-
-            if processing is True:
-                for index, breach in enumerate(breaches['data']):
-                    if (breach['domain']) != '':
-                        domain = "[" + str(breach['domain']) + "](https://" + str(breach['domain']) + ")"
-                    else:
-                        domain = "N/A"
-
-                    date_format_from = '%Y-%m-%d %H:%M:%S'
-                    date_format_to = '%a %d %B %Y'
-
-                    breach_date = datetime.strptime(str(breach['breach_date']), date_format_from)
-                    breach_date = datetime.strftime(breach_date, date_format_to)
-
-                    added_date = datetime.strptime(str(breach['added_date']), date_format_from)
-                    added_date = datetime.strftime(added_date, date_format_to)
-
-                    embedlist = discord.Embed(title='Breach #' + str(index+1), color = discord.Color.red())
-                    output = "Domain: " + domain + "\nOccured: " + breach_date + "\nReported: " + added_date
-                    embedlist.add_field(name=str(breach['title']), value=output, inline=False)
-
-                    await self.channel.send(embed=embedlist)
-                    await asyncio.sleep(1)
-                
-            await asyncio.sleep(5)
-
-        await self.channel.send('================ **BREACH SERVICE REPORT END** ================')
     @breach.before_loop
     async def before_breach(self):
         await self.bot.wait_until_ready()
